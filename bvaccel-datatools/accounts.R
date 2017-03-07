9e3eb677b2d49d1f723a20abc77296611efd7b56
mechellewarneke <mechellewarneke@gmail.com>
2017-03-07 06:43:50 +0000

    updates

--- bvaccel-datatools/accounts.R
+++ bvaccel-datatools/accounts.R
@@ -32,6 +32,14 @@ render3 <- reactive({
               vals$view$name)
 })
 
+dateRange <- reactive({
+  req(auth())
+  dateRangeInput('dateRange',
+                 label = 'Date range input: yyyy-mm-dd',
+                 start = Sys.Date() - 2, end = Sys.Date() + 2
+  )
+})
+
 renderUI({
   req(auth())
   dateRange()
