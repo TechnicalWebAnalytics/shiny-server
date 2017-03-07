dd5aaa31d47db479d8d9e6bb73e11503a442be58
mechellewarneke <mechellewarneke@gmail.com>
2017-03-07 07:27:05 +0000

    additional version control updates

--- bvaccel-datatools/staging/daterange.R
+++
@@ -1,7 +0,0 @@
-dateRange <- reactive({
-  req(auth())
-  dateRangeInput('dateRange',
-                 label = 'Date range input: yyyy-mm-dd',
-                 start = Sys.Date() - 2, end = Sys.Date() + 2
-  )
-})
